2429f641b4db82f2bbf4b46e52274d3c2c7749d6
Ana <anasilveiralorena@gmail.com>
2015-12-16 11:19:09 +0000

    Add rainfall module

---
+++ rainfall_conersion.py
@@ -0,0 +1,9 @@
+"""A library to perform rainfall unite conversions"""
+
+def inches_to_mm(inches):
+	"""convert inches to milimiters.
+	Arguments:
+	inches --the rainfall inches
+	"""
+	mm = inches * 25.4
+	return mm
